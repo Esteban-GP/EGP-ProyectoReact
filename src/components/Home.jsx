d2bca9c9ade1152a9bf917811b435b426a08fbf7
Esteban González <estebangp2003@gmail.com>
2025-02-06 15:00:43 +0100

    a

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import banderaf1 from '/banderaf1.gif';
 import Product from "../fragmentos/Product"
 
-const Home = () => {
+function Home() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        async function getProducts() {
+            try {
+                const response = await fetch('http://localhost:5000/products');
+                if (!response.ok) {
+                    throw new Error('Error fetching products');
+                }
+                const data = await response.json();
+                setProducts(data); // Assuming data is an array of products
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        }
+
+        getProducts();
+    }, []);
+
     return (
         <div>
             <div className="inset-0 flex flex-col items-center justify-center overflow-hidden bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${banderaf1})`, backgroundSize: 'cover', height: '100vh', backgroundPosition: 'center' }}>
@@ -12,6 +31,10 @@ const Home = () => {
             
             <h2 className='text-5xl text-center mt-20' style={{ fontFamily: 'Formula1Regular, sans-serif'}}>Highlighted Products</h2>
             <hr className='mb-20 mt-6 w-100 mx-auto ring-1' />
+            <div className='grid grid-cols-5 gap-4 mx-50 mb-50'>
+                {products.map(product => 
+                    <Product key={product.id} product={product} />)}
+            </div>
         </div>
     );
 };
--- src/fragmentos/Product.jsx
+++ src/fragmentos/Product.jsx
@@ -1,12 +1,12 @@
-import { Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-
-function Product() {
+function Product({ product }) {
     return (
-        <div className='container w-30'>
-
+        <div className='p-4 rounded-lg shadow-md bg-gray-50'>
+            <img src={product.img} alt={product.name} className="w-full h-70 object-cover rounded-md" />
+            <h3 className='text-xl font-bold mt-2'>{product.name}</h3>
+            <p className='text-gray-600 text-right'>{product.price} €</p>
         </div>
     );
 }
+
 
 export default Product;
