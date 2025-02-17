6ef22ab73edae42e04159ac695ca7fd882a2ab90
EstebanGP <estebangp2003@gmail.com>
2025-02-17 12:11:49 +0100

    teams medio hecho

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -21,17 +21,29 @@ function Team({ products, teams }) {
     return (
         <div>
             <div className="inset-0 flex flex-col items-center justify-center overflow-hidden bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${fondosimetrico})`, backgroundSize: 'cover', height: '100vh', backgroundPosition: 'center' }}>
-                <div key={team.id} className="text-center text-white">
-                    <img src={team.img} alt={team.name} className="w-1/2 mx-auto mb-4" />
-                    <p className="text-2xl">{team.description}</p>
+                <div className='flex flex-center'>
+                    <div className='mx-10'>
+                        <img src={team.d1img} className='w-110'/>
+                        <h2 className='text-2xl text-center mt-20 text-white' style={{ fontFamily: 'Formula1-Wide, sans-serif' }}>{team.d1name}</h2>
+                    </div>
+                   
+                    <div key={team.id} className="text-center text-white w-200 mx-20">
+                        <img src={team.img} alt={team.name} className="w-40 mx-auto mb-30" />
+                        <p className="text-2xl">{team.description}</p>
+                    </div>
+                    <div className='mx-10'>
+                        <img src={team.d2img} className='w-110' />
+                        <h2 className='text-2xl text-center mt-20 text-white' style={{ fontFamily: 'Formula1-Wide, sans-serif' }}>{team.d2name}</h2>
+                    </div>
                 </div>
+
             </div>
-            <h2 className='text-5xl text-center mt-20' style={{ fontFamily: 'Formula1Regular, sans-serif'}}>{team.name} Products</h2>
+            <h2 className='text-5xl text-center mt-20' style={{ fontFamily: 'Formula1Regular, sans-serif' }}>{team.name} Products</h2>
             <hr className='mb-20 mt-6 w-100 mx-auto ring-1' />
             <div className='grid grid-cols-5 gap-4 mx-50 mb-50'>
-                {products.map(product => 
-                    (product.team === team.nameId &&
-                        <Product key={product.id} product={product} />))
+                {products.map(product =>
+                (product.team === team.nameId &&
+                    <Product key={product.id} product={product} />))
                 }
             </div>
         </div>
